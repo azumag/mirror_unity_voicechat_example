47f80d6a9f70f0fe2a06acb3676133c5f3a20bf8
azumag <tsubasa.azumagakito@gmail.com>
2021-09-02 04:02:14 +0900

    add data compression for voicechat

--- Assets/VoiceChatManager.cs
+++ Assets/VoiceChatManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,8 +12,8 @@ public class VoiceChatManager : NetworkBehaviour
     public AudioSource audioSourceEar;
     // float[] audioData;
     byte[] audioData;
-    float[] audioFloat;
-    int sampleRate = 3000;
+    // float[] audioFloat;
+    int sampleRate = 22500;
 
     // Start is called before the first frame update
     void Start()
@@ -33,52 +35,101 @@ public class VoiceChatManager : NetworkBehaviour
         if (!audioSource.isPlaying) {
             // audioSource.Play();
         }
-        Debug.Log("test");
-        // if (audioData != null) {
-            // CmdSendAudio(audioData);
-        // }
-        ;
-        if (audioFloat != null) {
-            CmdSendAudio(audioFloat);
+        if (audioData != null) {
+            CmdSendAudio(audioData);
         }
+        // if (audioFloat != null) {
+        //     CmdSendAudio(audioFloat);
+        // }
     }
 
     void Update() {
         if (!isLocalPlayer) return;
         float[] floatData = new float[sampleRate];
         audioSource.clip.GetData(floatData, 0); 
-        audioFloat = floatData;
+
+        // convert to byte array
+        byte[] byteData = new byte[floatData.Length * 4];
+        Buffer.BlockCopy(floatData, 0, byteData, 0, byteData.Length);
+        audioData = byteData;
+
+        // audioFloat = floatData;
         // float[] floatData = new float[audioSource.clip.samples * audioSource.clip.channels];
    //     Debug.Log("floatData;;;" + floatData.Length);
     }
 
     [Command(channel = Channels.Unreliable)]
-    // private void CmdSendAudio(byte[] _data)
-    private void CmdSendAudio(float[] _data)
+    private void CmdSendAudio(byte[] _data)
+    // private void CmdSendAudio(float[] _data)
     {
-        Debug.Log("CmdSend");
-        RpcSendAudio(_data);
+        byte[] compressedData = Compress(_data);
+        Debug.Log("CmdSend: " + compressedData.Length);
+        RpcSendAudio(compressedData);
 	}
 
      // "includeOwner" player should apply their own data, and not wait to send and recieve it back
-    // [ClientRpc(channel = Channels.Unreliable)]
-    [ClientRpc(channel = Channels.Unreliable, includeOwner = false)]
-    // private void RpcSendAudio(byte[] _data)
-    private void RpcSendAudio(float[] _data)
+    [ClientRpc(channel = Channels.Unreliable)]
+    // [ClientRpc(channel = Channels.Unreliable, includeOwner = false)]
+    private void RpcSendAudio(byte[] _data)
+    // private void RpcSendAudio(float[] _data)
     {
         // if (_data.Length > 0)  {
         //     // Debug.Log("RPCSend " + _data[0]);
         // }
-        Debug.Log("RpcSend " + _data[0]);
+        // Debug.Log("RpcSend " + _data[0]);
         Debug.Log("RpcSend " + audioSourceEar.isPlaying);
         if (!audioSourceEar.isPlaying) {
             if (audioSourceEar.clip != null) {
-                audioSourceEar.clip.SetData(_data, 0);
+                float[] floatData = ToFloatArray(Decompress(_data));
+                audioSourceEar.clip.SetData(floatData, 0);
                 audioSourceEar.Play();
             } else {
-                audioSourceEar.clip = AudioClip.Create("ear", sampleRate, 1, sampleRate, false);
+				audioSourceEar.clip = AudioClip.Create("ear", sampleRate, 1, sampleRate, false);
             }
         }
+    }
+
+    public static byte[] Compress(byte[] src)
+    {
+        using (var ms = new MemoryStream())
+        {
+            using (var ds = new DeflateStream(ms, CompressionMode.Compress, true/*msは*/))
+            {
+                ds.Write(src, 0, src.Length);
+            }
+
+            // 圧縮した内容をbyte配列にして取り出す
+            ms.Position = 0;
+            byte[] comp = new byte[ms.Length];
+            ms.Read(comp, 0, comp.Length);
+            return comp;
+        }
+    }
+
+    public static byte[] Decompress(byte[] src)
+    {
+        using (var ms = new MemoryStream(src))
+        using (var ds = new DeflateStream(ms, CompressionMode.Decompress))
+        {
+            using (var dest = new MemoryStream())
+            {
+                ds.CopyTo(dest);
+
+                dest.Position = 0;
+                byte[] decomp = new byte[dest.Length];
+                dest.Read(decomp, 0, decomp.Length);
+                return decomp;
+            }
+        }
+    }
+
+    public float[] ToFloatArray(byte[] byteArray) {
+        int len = byteArray.Length / 4;
+        float[] floatArray = new float[len];
+        for (int i = 0; i < byteArray.Length; i+=4) {
+            floatArray[i/4] = System.BitConverter.ToSingle(byteArray, i);
+        }
+        return floatArray;
     }
 
     // void OnAudioFilterRead(float[] data, int channels) {
